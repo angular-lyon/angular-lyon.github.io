937e826a29f374e2ed4b801474d0cd3209a0d8f8
Edouard Bozon <bozonedouard@gmail.com>
2021-04-27 18:21:00 +0200

    Fix deploy

--- server.ts
+++ server.ts
@@ -11,7 +11,7 @@ import { existsSync } from 'fs';
 export function app(): express.Express {
   const server = express();
   const distFolder = join(process.cwd(), 'dist/angular-lyon/browser');
-  const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const indexHtml = existsSync(join(distFolder, 'index.html')) ? 'index.html' : 'index.original.html';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
   server.engine('html', ngExpressEngine({
